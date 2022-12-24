c36c133598abf9fe76bd2978f09099c10ffd9fe8
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-12-24 17:36:20 +0100

    (Mixed Changes) - Implementation of Role Entity and RoleRepository Test case
    (Mixed Changes) - Implementation of Role Entity and RoleRepository Test case
    1. Pom.xml Udpate Changes
    2. Entity Scan Changes

---
+++ ShopmeCommon/src/main/java/com/shopme/entity/Role.java
@@ -0,0 +1,90 @@
+package com.shopme.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+
+	@Column(length = 40, nullable = false, unique = true)
+	private String name;
+
+	@Column(length = 140, nullable = false)
+	private String description;
+
+	public Role() {
+	}
+
+	public Role(Integer id) {
+		this.Id = id;
+	}
+
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Role(String name, String description) {
+		super();
+		this.name = name;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(Id, other.Id);
+	}
+
+	@Override
+	public String toString() {
+		return "Role [name=" + name + "]";
+	}
+
+}
