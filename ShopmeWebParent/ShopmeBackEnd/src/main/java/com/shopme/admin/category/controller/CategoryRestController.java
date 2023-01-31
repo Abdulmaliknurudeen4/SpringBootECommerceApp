b081089e0d5b52124bbe7f344b04d9c6d0aa8b5d
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2023-01-31 18:32:12 +0100

    Categories Module Adding the Category Rest Controller for checking Duplicate Names

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/controller/CategoryRestController.java
@@ -0,0 +1,19 @@
+package com.shopme.admin.category.controller;
+
+import com.shopme.admin.category.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryRestController {
+
+    @Autowired
+    private CategoryService service;
+
+    @PostMapping("categories/check_name")
+    public String checkDuplicateCategory(@Param("name") String name, @Param("id") Integer id) {
+        return service.isCategoryUnique(id, name) ? "OK" : "Duplicated";
+    }
+}
