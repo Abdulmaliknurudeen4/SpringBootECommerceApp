78e221c6a835e9c7d12cdc26aca0f764f43423e9
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2023-03-16 01:15:13 +0100

    Product Module (FrontEnd) : Coding Index Client Page and Custom Error Pages.

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.shopme.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.Serializable;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig implements Serializable {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests().anyRequest().permitAll().and().build();
+    }
+
+}
