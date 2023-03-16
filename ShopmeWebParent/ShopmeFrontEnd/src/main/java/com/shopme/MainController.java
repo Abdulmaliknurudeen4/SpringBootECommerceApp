8ff85b34710eac20d129a7eceb6b404d39290ae1
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2023-03-16 02:11:49 +0100

    Product Module (FrontEnd) : Listing of Categories. All Dynamic content have Bugs and Not Rendering

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
@@ -1,14 +1,28 @@
 package com.shopme;
 
+import com.shopme.category.CategoryService;
+import com.shopme.entity.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
-	
-	@GetMapping("")
-	public String viewHomePage() {
-		return "index";
-	}
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
+    private CategoryService categoryService;
+
+
+    @GetMapping("")
+    public String viewHomePage(Model model) {
+        List<Category> categoryList = categoryService.listNoChildrenCategories();
+        model.addAttribute("categories", categoryList);
+        return "index";
+    }
 
 }
